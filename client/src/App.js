e62d51e5dd54517b78096d45981362081fa5463a
KD3-80151 <sagarkumarchhotu@ymail.com>
2024-04-17 17:51:59 +0530

    Revised Completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './style.css'; 
 
-function App() {
+
+
+const HorizontalScaleComponent = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:5000/data');
+        setData(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="horizontal-scale">
+    {data.map(item => (
+      <div key={item._id} className="horizontal-bar">
+        {item.ts} - {item.vibration}
+      </div>
+    ))}
+  </div>
   );
 }
-
-export default App;
+  
+export default HorizontalScaleComponent;
